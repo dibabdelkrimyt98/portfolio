52482e6247c511c7cdcc4c6b452d00eda689fb0f
dibabdelkrimyt98 <dibabdelkrimyt98@gmail.com>
2025-12-31 12:38:22 +0800

    new changes in works enverenements

--- src/data/worksData.js
+++ src/data/worksData.js
@@ -1,22 +1,23 @@
 export const worksData = [
   {
     id: 1,
-    title: "Eco-Tracker App",
-    date: "Mars 2024",
-    videoSrc: "/videos/clinique.mp4",
+    title: "Site EURL Pomaria",
+    date: "Octobre 2025",
+    videoSrc: "/videos/pomaria.mp4",
   },
   {
     id: 2,
-    title: "Fintech Dashboard",
-    date: "Janvier 2024",
-    videoSrc: "/videos/project2.mp4",
+    title: "Site vitrine Clinique Dentale",
+    date: "Novembre 2025",
+    videoSrc: "/videos/clinique.mp4",
   },
   {
     id: 3,
-    title: "E-commerce Mobile",
-    date: "Novembre 2023",
-    videoSrc: "/videos/project3.mp4",
+    title: "Site EB Garage",
+    date: "Decembre 2025",
+    videoSrc: "/videos/ebgarage.mp4",
   },
+  
   {
     id: 4,
     title: "E-commerce Mobile",
